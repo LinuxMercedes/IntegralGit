3595bffb71f415999847f323af36737a41ce4b56
LinuxMercedes <LinuxMercedes@gmail.com>
2012-06-17 22:52:14 -0500

    Add code to show last commit message

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ lastCommit = "No recorded commits!"
 def hello():
   return "IntegralGit: continuous integration via GitHub"
 
+@app.route("/latest")
+def latest():
+  return lastCommit
+
 @app.route("/update", methods=["POST"])
 def update():
-  print json.dumps(request.form['payload'])
-  return
+  payload = json.dumps(request.form['payload'])
+  lastCommit = payload['commits'][0]['message']
+  return ""
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", debug=True)
