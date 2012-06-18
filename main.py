680813d636cc52c8d7720005c8ba0d4459b32853
LinuxMercedes <LinuxMercedes@gmail.com>
2012-06-17 23:51:50 -0500

    Adding some config crap; probably not going to need it

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from pprint import pprint
 import json
+import ConfigParser
 
 app = Flask(__name__)
 lastCommit = "No recorded commits!"
@@ -18,6 +19,13 @@ def update():
   payload = json.dumps(request.form['payload'])
   lastCommit = payload['commits'][0]['message']
   return ""
+
+def parseConfig(filename):
+  config = ConfigParser.SafeConfigParser()
+  config.read(filename)
+
+  for section in config.sections():
+    lastCommit[section] = "No recorded commits!"
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", debug=True)
